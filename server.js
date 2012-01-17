454d6d985d8d8037d079f1995a645eaaf11ba7e5
Matthew O'Donoghue <mattyod@gmail.com>
2012-01-17 11:15:27 +0000

    added simple ajax support

--- server.js
+++ server.js
@@ -19,22 +19,39 @@ app.get('*', function(req, res) {
 
   req.url = req.url === '/'? '/index.html' : req.url;
   req.url = baseUrl + req.url;
-  
-  path.exists(req.url, function(exists) {
-    if(exists) {
-        console.log('Serving: ', req.url);
 
-        serveFile(res, req.url);
-        
-    } else {
-      console.log('File does not exist: ', req.url);
+  // Is it an AJAX request?
+  if(req.xhr) {
+    // Clear out the jQuery params - q&d
+    req.url = req.url.split(/\?/)[0];
+    path.exists(req.url, function(exists) {
+      if(exists) {
+        fs.readFile(req.url, 'binary', function(err, data) {
+          if(err) {
+            console.log('AJAX requested file cannot be read', err);
+            return;
+          } else {
+            res.send(data);
+          }
+        });
+      }
       
-      serveFile(res, baseUrl + '/errors/404.html');
-    }
-  });
-});
+    });
+  } else {
+    path.exists(req.url, function(exists) {
+      if(exists) {
+          console.log('Serving: ', req.url);
 
-app.listen(3000);
+          serveFile(res, req.url);
+          
+      } else {
+        console.log('File does not exist: ', req.url);
+        
+        serveFile(res, baseUrl + '/errors/404.html');
+      }
+    });
+  }
+}).listen(1337);
 
 // We are up...
-console.log('Server running on port 3000');
+console.log('Server running on port 1337');
